3cb2d7f125b2dcbe6f53d09e6e6bd3980678e29c
Rob Halff <rob.halff@gmail.com>
2013-12-17 15:18:17 +0100

    some structure checking

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 
 var file = './complex.fbp';
+//var file = './parseme.fbp';
 var fbpData = fs.readFileSync(file).toString('utf-8');
 var Parser = require('./lib/parser');
 
