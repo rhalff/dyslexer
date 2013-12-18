3152b740f91f471ff81a5a195d617903622d9c6f
Rob Halff <rob.halff@gmail.com>
2013-12-18 15:12:47 +0100

    squeeze in scope

--- main.js
+++ main.js
@@ -1,9 +1,1 @@
-var fs = require('fs');
-
-var file = './complex.fbp';
-//var file = './parseme.fbp';
-var fbpData = fs.readFileSync(file).toString('utf-8');
-var Parser = require('./lib/parser');
-
-var parser = new Parser();
-var res = parser.parse(fbpData);
+module.exports = require('./lib/dyslexer');
